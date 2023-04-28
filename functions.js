fdabd4705cb692b9ae3dde9a2e69faa9826b06cd
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-04-28 09:31:44 -0700

    Problems 1, 2, and 3 solved.

---
+++ functions.js
@@ -0,0 +1,43 @@
+// Write a function that takes two numbers as arguments and returns their sum.
+
+// function sum2nums(num1, num2) {
+//   return num1 + num2;
+// }
+
+// sum2nums(5, 10);
+
+// Write a function that takes a string as an argument and returns the length of the string.
+
+// function strLength(str1) {
+//   return str1.length;
+// }
+
+// strLength("banana");
+// Write a function that takes an array of numbers as an argument and returns the largest number in the array.
+
+// let arr = [1, 2, 3, 4, 5];
+// let largest = 0;
+
+// function arrLargest(arr) {
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//       largest = arr[i];
+//     }
+//   }
+//   return largest;
+// }
+
+// arrLargest(arr);
+// Write a function that takes an array of strings as an argument and returns a new array with all the strings capitalized.
+
+// Write a function that takes a number as an argument and returns true if the number is even, and false if it is odd.
+
+// Write a function that takes two arrays as arguments and returns a new array that contains all the elements from both arrays, in the order they appear.
+
+// Write a function that takes a string as an argument and returns a new string with all the vowels removed.
+
+// Write a function that takes an array of numbers as an argument and returns a new array with all the numbers doubled.
+
+// Write a function that takes an array of strings as an argument and returns a new array with all the strings sorted in alphabetical order.
+
+// Write a function that takes a number as an argument and returns the factorial of that number (i.e., the product of all the numbers from 1 to that number).
