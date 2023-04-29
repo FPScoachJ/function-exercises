e41f54441effd70c3742f693f262a20a2fac625b
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-04-28 17:33:54 -0700

    All problems solved
    Uploading...

--- functions.js
+++ functions.js
@@ -64,22 +64,58 @@
 // function mergeArray(arr1, arr2){
 //     arr3 = arr1.concat(arr2);
 //     return arr3;
-// }   
+// }
 
 // mergeArray(arr1, arr2);
 
 // Write a function that takes a string as an argument and returns a new string with all the vowels removed.
 
-let str = "banana";
+// let str = "banana";
 
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, "");
-}
+// function removeVowels(str) {
+//   return str.replace(/[aeiou]/gi, "");
+// }
 
-removeVowels(str);
+// removeVowels(str);
 
 // Write a function that takes an array of numbers as an argument and returns a new array with all the numbers doubled.
 
+// let arr = [1, 2, 3, 4, 5];
+
+// function arrDouble(arr) {
+//   let arr2 = [];
+//   for (i = 0; i < arr.length; i++) {
+//     arr2.push(arr[i] * 2);
+//   }
+//   return arr2;
+// }
+
+// arrDouble(arr);
+
 // Write a function that takes an array of strings as an argument and returns a new array with all the strings sorted in alphabetical order.
 
+// let arr1 = ["z", "y", "x", "w", "v"];
+
+// function arrSort(arr1) {
+//   let arr2 = [];
+//   arr2.push(arr1.sort());
+//   return arr2;
+// }
+
+// arrSort(arr1);
+
 // Write a function that takes a number as an argument and returns the factorial of that number (i.e., the product of all the numbers from 1 to that number).
+
+// function factorial(n) {
+//   let answer = 1;
+//   if (n === 0 || n === 1) {
+//     return answer;
+//   } else if (n > 1) {
+//     for (let i = n; i >= 1; i--) {
+//       answer = answer * i;
+//     }
+//     return answer;
+//   }
+// }
+// let n = 5;
+// factorial(n);
